0e5f58849a543e379f5c2d95eb50a823812a7dfa
Elvis2280 <Elvismiranda924g@gmail.com>
2024-02-28 22:31:47 +0900

    debug

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "gametracker/docs"
+	"gametracker/routes"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -24,24 +25,23 @@ func main() {
 	r := gin.Default()
 	r.Use(gin.Logger())
 
-	// Create a new Fizz instance from the Gin engine.
 	f := fizz.NewFromEngine(r)
 
-	// Add Open API description
+	// OpenAPI infos
 	infos := &openapi.Info{
 		Title:       "Game tracker API",
 		Description: "This is my Service API",
 		Version:     "0.1",
 	}
 
-	// Create an endpoint for openapi.json file
+	// open api routes
 	f.GET("/openapi.json", nil, f.OpenAPI(infos, "json"))
-	//// Now add a UI handler
 	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	//routes.SetupTagsRoutes(r)
-	//routes.SetupPlatformsRoutes(r)
-	//routes.SetupGamesRoutes(r)
-	//routes.SetupUsersRoutes(r)
+	// app routes
+	routes.SetupTagsRoutes(r)
+	routes.SetupPlatformsRoutes(r)
+	routes.SetupGamesRoutes(r)
+	routes.SetupUsersRoutes(r)
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
