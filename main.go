c078e9a956dfb77ebfa5838bd90c5d0613787935
Elvis2280 <Elvismiranda924g@gmail.com>
2024-02-28 22:24:32 +0900

    debug

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "gametracker/docs"
 	"github.com/gin-gonic/gin"
 	"github.com/wI2L/fizz"
+	"github.com/wI2L/fizz/openapi"
 )
 
 // @title Game tracker API
@@ -25,11 +26,11 @@ func main() {
 	f := fizz.NewFromEngine(r)
 
 	// Add Open API description
-	//infos := &openapi.Info{
-	//	Title:       "Game tracker API",
-	//	Description: "This is my Service API",
-	//	Version:     "0.1",
-	//}
+	infos := &openapi.Info{
+		Title:       "Game tracker API",
+		Description: "This is my Service API",
+		Version:     "0.1",
+	}
 
 	// Create an endpoint for openapi.json file
 	f.GET("/openapi.json", nil, f.OpenAPI(infos, "json"))
